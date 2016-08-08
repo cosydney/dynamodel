a9d091e035146597209519d7c01c5e2d64d5d9fe
Lina Garcia <garcia.j.lina@gmail.com>
2016-08-08 17:54:25 +0100

    add controller with routes

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,49 @@
+class ClientsController < ApplicationController
+
+  before_action :find_client, only: [:new, :create, :edit]
+  before_action :set_client, only: [:show, :update]
+
+  def show
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+
+    if @client.save
+      redirect_to client_path(@client)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @client = Client.new
+  end
+
+  def update
+    if @client.update(client_params)
+      redirect_to client_path(@client)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def set_client
+    @client = Client.new(client_params)
+  end
+
+  def find_client
+    @client = Client.find(params[:id])
+  end
+
+  def client_params
+    params.require(:client).permit(:company_name, :website, :user_id) #how to pass user_id
+  end
+
+end
