8feb0d1a7e7e5e7d1100311d4b35fff04586cd3b
joao viana <j.bernardo.viana@hotmail.com>
2016-08-08 16:35:36 +0100

    First routes defined and commented; Photos destroyed when Mannequin is destroyed

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,53 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :mannequins do
+    resources :photos
+  end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :clients, only: [:new, :create, :show, :edit, :update]
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+#----------------------------------------ROUTES---------------------------------------------
+#                     Prefix Verb   URI Pattern                                         Controller#Action
+#         new_user_session GET    /users/sign_in(.:format)                            devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)                            devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)                           devise/sessions#destroy
+#            user_password POST   /users/password(.:format)                           devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)                       devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format)                      devise/passwords#edit
+#                          PATCH  /users/password(.:format)                           devise/passwords#update
+#                          PUT    /users/password(.:format)                           devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)                             devise/registrations#cancel
+#        user_registration POST   /users(.:format)                                    devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)                            devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)                               devise/registrations#edit
+#                          PATCH  /users(.:format)                                    devise/registrations#update
+#                          PUT    /users(.:format)                                    devise/registrations#update
+#                          DELETE /users(.:format)                                    devise/registrations#destroy
+#                     root GET    /                                                   pages#home
+#         mannequin_photos GET    /mannequins/:mannequin_id/photos(.:format)          photos#index
+#                          POST   /mannequins/:mannequin_id/photos(.:format)          photos#create
+#      new_mannequin_photo GET    /mannequins/:mannequin_id/photos/new(.:format)      photos#new
+#     edit_mannequin_photo GET    /mannequins/:mannequin_id/photos/:id/edit(.:format) photos#edit
+#          mannequin_photo GET    /mannequins/:mannequin_id/photos/:id(.:format)      photos#show
+#                          PATCH  /mannequins/:mannequin_id/photos/:id(.:format)      photos#update
+#                          PUT    /mannequins/:mannequin_id/photos/:id(.:format)      photos#update
+#                          DELETE /mannequins/:mannequin_id/photos/:id(.:format)      photos#destroy
+#               mannequins GET    /mannequins(.:format)                               mannequins#index
+#                          POST   /mannequins(.:format)                               mannequins#create
+#            new_mannequin GET    /mannequins/new(.:format)                           mannequins#new
+#           edit_mannequin GET    /mannequins/:id/edit(.:format)                      mannequins#edit
+#                mannequin GET    /mannequins/:id(.:format)                           mannequins#show
+#                          PATCH  /mannequins/:id(.:format)                           mannequins#update
+#                          PUT    /mannequins/:id(.:format)                           mannequins#update
+#                          DELETE /mannequins/:id(.:format)                           mannequins#destroy
+#                  clients POST   /clients(.:format)                                  clients#create
+#               new_client GET    /clients/new(.:format)                              clients#new
+#              edit_client GET    /clients/:id/edit(.:format)                         clients#edit
+#                   client GET    /clients/:id(.:format)                              clients#show
+#                          PATCH  /clients/:id(.:format)                              clients#update
+#                          PUT    /clients/:id(.:format)                              clients#update
+# ---------------------------------------------------------------------------------------------------------------------------
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
