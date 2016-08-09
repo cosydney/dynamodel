d0d1300146c49446d40b1a52214ab507aa7d1c3b
Sydney Cohen <chnsydney@gmail.com>
2016-08-09 12:23:59 +0100

    drop photos table since we don't need it because of attachinary

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809104708) do
+ActiveRecord::Schema.define(version: 20160809112141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,17 +74,6 @@ ActiveRecord::Schema.define(version: 20160809104708) do
   add_index "photo_shootings", ["client_id"], name: "index_photo_shootings_on_client_id", using: :btree
   add_index "photo_shootings", ["mannequin_id"], name: "index_photo_shootings_on_mannequin_id", using: :btree
 
-  create_table "photos", force: :cascade do |t|
-    t.integer  "mannequin_id"
-    t.string   "profile_picture"
-    t.string   "polaroid"
-    t.string   "portofolio"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "photos", ["mannequin_id"], name: "index_photos_on_mannequin_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -115,5 +104,4 @@ ActiveRecord::Schema.define(version: 20160809104708) do
   add_foreign_key "mannequins", "users"
   add_foreign_key "photo_shootings", "clients"
   add_foreign_key "photo_shootings", "mannequins"
-  add_foreign_key "photos", "mannequins"
 end
