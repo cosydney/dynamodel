9d6ea772e632174a3bb58164e7ef8a282e89b4c9
Sydney Cohen <chnsydney@gmail.com>
2016-08-09 18:05:44 +0100

    css for file upload is better now :)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
+//= require init_attachinary
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require attachinary_template
