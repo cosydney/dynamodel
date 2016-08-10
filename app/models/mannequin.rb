0200f4c88ace111ad24378a6b897bd94532cb6a3
joao viana <j.bernardo.viana@hotmail.com>
2016-08-10 12:37:06 +0100

    Solved conflict

--- app/models/mannequin.rb
+++ app/models/mannequin.rb
@@ -3,11 +3,8 @@ class Mannequin < ActiveRecord::Base
   has_many :attachinary_files, dependent: :destroy
   has_many :photo_shootings
   has_many :clients, through: :photo_shootings
-<<<<<<< HEAD
 
   validates :age, presence: true, on: :update
-=======
-  validates :age, presence: true
   has_attachments :photos
->>>>>>> 78574f593424cf2d33ca0486efaab1831f996c82
+
 end
